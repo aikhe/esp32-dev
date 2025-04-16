c5f85cc51ff3d73f59b11140015850bb69a61bc2
aikhe <ikeandrie.ro@gmail.com>
2025-04-16 16:41:03 +0800

    feat(supabase): http GET req for supabase endpoint + testing getNumber() for fetching resident numbers

--- supabase/supabase.ino
+++ supabase/supabase.ino
@@ -1,9 +1,66 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
 
+#define SSID              "TK-gacura"
+#define PASSWORD          "gisaniel924"
+
+#define supabaseUrl       "https://jursmglsfqaqrxvirtiw.supabase.co"
+#define supabaseKey       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1cnNtZ2xzZnFhcXJ4dmlydGl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3ODkxOTEsImV4cCI6MjA2MDM2NTE5MX0.ajGbf9fLrYAA0KXzYhGFCTju-d4h-iTYTeU5WfITj3k"
+#define tableName         "resident_number"
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi\n");
+
+  getNumbers()
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
 
+void getNumbers()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi not connected. Attempting to reconnect...");
+    while (!WiFi.reconnect())
+    {
+      Serial.println("Reconnecting to WiFi...");
+      delay(500);
+    }
+    Serial.println("WiFi reconnected.");
+  }
+
+  HTTPClient http;
+  String endpoint = String(supabaseUrl) + "/rest/v1/" + tableName + "?id=eq.1";
+  http.begin(endpoint);
+
+  http.addHeader("apikey", supabaseKey);
+  http.addHeader("Authorization", "Bearer " + String(supabaseKey));
+
+  int httpResponseCode = http.GET();
+  if (httpResponseCode > 0)
+  {
+    Serial.println("\nReceiving:");
+    String response = http.getString();
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    Serial.println("Response: " + response);
+  }
+  else
+  {
+    Serial.println("Error in HTTP request");
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    String response = http.getString();
+    Serial.println("Response: " + response);
+  }
+  http.end();
+  Serial.println("\n============================================\n");
 }
